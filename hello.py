46ae2cdaa0bf8cfefec3fce44bfa71621e0fdadf
Carol Pinheiro <caroline.bpinheiro@outlook.com>
2023-09-05 09:42:23 -0300

    feat: added metadata and multiline comments

--- hello.py
+++ hello.py
@@ -1,3 +1,22 @@
 #!/usr/bin/env python3
 
+"""Hello World multilínguas
+
+Dependendo da língua configurada no ambiente, o programa exibe a 
+correspondente.
+
+Como usar:
+    export LANG=pt_BR
+
+Execução:
+    $ python3 hello.py
+    ou
+    $ ./hello.py
+"""
+__version__ = "0.0.1"
+__author__ = "Caroline Pinheiro"
+__license__ = "Unlicense"
+
+
+# Este programa imprime Hello World!
 print("Hello World!")
